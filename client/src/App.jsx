7f06a10bbe0dc7ff369b9a2b84dd35e44f324ce6
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-12-04 19:51:47 +0100

    Add base components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
+import Footer from "./components/Footer.jsx"
+import Header from "./components/Header.jsx"
+import Pagination from "./components/Pagination.jsx"
+import Search from "./components/Search.jsx"
+import UserList from "./components/UserList.jsx"
+
 function App() {
 
   return (
     <div>
-      <h1>Hello</h1>
-    </div>
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+
+          <UserList />
+
+          <Pagination />
+        </section>
+
+
+      </main>
+      <Footer />
+    </div >
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="header" >
+            <div className="logo">
+                <span classname="course">React Exercise - Components Deep Dive</span>
+            </div>
+        </header>
+    )
+}
